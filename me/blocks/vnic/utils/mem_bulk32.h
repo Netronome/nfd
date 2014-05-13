745ea6f8e4f91913d7ffb8bcf46440e66c64faf7
Michael Rapson <michael.rapson@netronome.com>
2014-05-13 09:58:19 +0200

    [me] Correct method names in temporary mem_bulk header

--- me/blocks/vnic/utils/mem_bulk32.h
+++ me/blocks/vnic/utils/mem_bulk32.h
@@ -11,7 +11,7 @@
 #include <types.h>
 
 /**
- * Read a multiple of 8B from a memory unit (EMEM, IMEM, or CTM)
+ * Read a multiple of 4B from a memory unit (EMEM, IMEM, or CTM)
  * @param data      pointer to sufficient read transfer registers for the op
  * @param addr      40-bit pointer to the memory start address
  * @param size      size of the read, must be a multiple of 8
@@ -23,21 +23,21 @@
  * alignment is assumed about address when converting from a 40bit pointer to
  * "src_op" fields in the __asm command, which makes the method general, but
  * suboptimal if it is guaranteed to be 256B aligned.  There is currently
- * limited support for size to be a runtime value, and for reads >64B, see
- * THSDK-1161.  mem_read64() provides a simplified interface where size is
+ * limited support for size to be a runtime value, and for reads >32B, see
+ * THSDK-1161.  mem_read32() provides a simplified interface where size is
  * assumed to be compile time constant, and the context swaps on an internal
  * signal while waiting for the read to complete.
  */
-__intrinsic void __mem_read64(__xread void *data, __dram void *addr,
+__intrinsic void __mem_read32(__xread void *data, __dram void *addr,
                               size_t size, const size_t max_size,
                               sync_t sync, SIGNAL *sig);
 
-__intrinsic void mem_read64(__xread void *data, __dram void *addr,
+__intrinsic void mem_read32(__xread void *data, __dram void *addr,
                             const size_t size);
 
 
 /**
- * Write a multiple of 8B to a memory unit (EMEM, IMEM, or CTM)
+ * Write a multiple of 4B to a memory unit (EMEM, IMEM, or CTM)
  * @param data      pointer to sufficient write transfer registers for the op
  * @param addr      40-bit pointer to the memory start address
  * @param size      size of the write, must be a multiple of 8
@@ -49,16 +49,16 @@ __intrinsic void mem_read64(__xread void *data, __dram void *addr,
  * alignment is assumed about address when converting from a 40bit pointer to
  * "src_op" fields in the __asm command, which makes the method general, but
  * suboptimal if it is guaranteed to be 256B aligned.  There is currently
- * limited support for size to be a runtime value, and for reads >64B, see
- * THSDK-1161.  mem_write64() provides a simplified interface where size is
+ * limited support for size to be a runtime value, and for reads >32B, see
+ * THSDK-1161.  mem_write32() provides a simplified interface where size is
  * assumed to be compile time constant, and the context swaps on an internal
  * signal while waiting for the write to complete.
  */
-__intrinsic void __mem_write64(__xwrite void *data, __dram void *addr,
+__intrinsic void __mem_write32(__xwrite void *data, __dram void *addr,
                                size_t size, const size_t max_size,
                                sync_t sync, SIGNAL *sig);
 
-__intrinsic void mem_write64(__xwrite void *data, __dram void *addr,
+__intrinsic void mem_write32(__xwrite void *data, __dram void *addr,
                              const size_t size);
 
 #endif /* !_NFP__MEM_BULK32_H_ */
