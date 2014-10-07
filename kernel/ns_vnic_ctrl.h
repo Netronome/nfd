f3913faeb1ebd6b0e6bca009b54308b22ddcf6c2
Rolf Neugebauer <rolf.neugebauer@netronome.com>
2014-10-07 16:53:33 +0100

    [netdev] add support for filtering on Ethernet multicast addresses
    resolves://SB-34

--- kernel/ns_vnic_ctrl.h
+++ kernel/ns_vnic_ctrl.h
@@ -1,5 +1,17 @@
 /*
- * Copyright (C) 2014 Netronome Systems, Inc. All rights reserved.
+ * Copyright 2014 Netronome, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * vim:shiftwidth=8:noexpandtab
  *
@@ -45,13 +57,15 @@
 #define NS_VNIC_CFG_CTRL		0x0000
 #define   NS_VNIC_CFG_CTRL_ENABLE	  (0x1 <<  0) /* Global enable */
 #define   NS_VNIC_CFG_CTRL_PROMISC	  (0x1 <<  1) /* Enable Promisc mode */
-#define   NS_VNIC_CFG_CTRL_RXCSUM	  (0x1 <<  2) /* Enable RX Checksum */
-#define   NS_VNIC_CFG_CTRL_TXCSUM	  (0x1 <<  3) /* Enable TX Checksum */
-#define   NS_VNIC_CFG_CTRL_RXVLAN	  (0x1 <<  4) /* Enable VLAN strip */
-#define   NS_VNIC_CFG_CTRL_TXVLAN	  (0x1 <<  5) /* Enable VLAN insert */
-#define   NS_VNIC_CFG_CTRL_SCATTER	  (0x1 <<  6) /* Scatter DMA */
-#define   NS_VNIC_CFG_CTRL_GATHER	  (0x1 <<  7) /* Gather DMA */
-#define   NS_VNIC_CFG_CTRL_LSO		  (0x1 <<  8) /* LSO/TSO */
+#define   NS_VNIC_CFG_CTRL_L2BC		  (0x1 <<  2) /* Allow L2 Broadcast */
+#define   NS_VNIC_CFG_CTRL_L2MC		  (0x1 <<  3) /* Allow L2 Multicast */
+#define   NS_VNIC_CFG_CTRL_RXCSUM	  (0x1 <<  4) /* Enable RX Checksum */
+#define   NS_VNIC_CFG_CTRL_TXCSUM	  (0x1 <<  5) /* Enable TX Checksum */
+#define   NS_VNIC_CFG_CTRL_RXVLAN	  (0x1 <<  6) /* Enable VLAN strip */
+#define   NS_VNIC_CFG_CTRL_TXVLAN	  (0x1 <<  7) /* Enable VLAN insert */
+#define   NS_VNIC_CFG_CTRL_SCATTER	  (0x1 <<  8) /* Scatter DMA */
+#define   NS_VNIC_CFG_CTRL_GATHER	  (0x1 <<  9) /* Gather DMA */
+#define   NS_VNIC_CFG_CTRL_LSO		  (0x1 << 10) /* LSO/TSO */
 #define   NS_VNIC_CFG_CTRL_RINGCFG	  (0x1 << 16) /* Ring runtime changes */
 #define   NS_VNIC_CFG_CTRL_RSS		  (0x1 << 17) /* RSS */
 #define   NS_VNIC_CFG_CTRL_IRQMOD	  (0x1 << 18) /* Interrupt moderation */
