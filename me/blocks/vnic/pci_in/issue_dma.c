087d82c73485225a02c0f93cc4c8176ef05e5b0a
Michael Rapson <michael.rapson@netronome.com>
2016-07-27 14:46:14 +0200

    [PCI.IN] wip://NFDH-191 Test whether queue is up before executing LSO function * The test for the LSO flag occurs at the start of packet processing, so we   do not know whether the queue is up or down at that stage.  Therefore,   after we have tested whether the descriptor is LSO we need to also check   whether the queue is down.  If it is down, falling through to the remainder   of the if-else statement is sufficient. * An "if (is_lso(desc) && is_up(queue))" style test is efficient because only   the "is_lso(desc)" test happens on the fast path (due to NFCC   short-curcuiting the test if the first stage fails).

--- me/blocks/vnic/pci_in/issue_dma.c
+++ me/blocks/vnic/pci_in/issue_dma.c
@@ -1018,8 +1018,10 @@ DECLARE_PROC_LSO(7);
  * is not advertised on either the PF or VFs. */
 #if ((NFD_CFG_VF_CAP & NFP_NET_CFG_CTRL_LSO) || \
      (NFD_CFG_PF_CAP & NFP_NET_CFG_CTRL_LSO))
-#define _ISSUE_PROC_LSO_TEST(_pkt)                  \
-    (tx_desc.pkt##_pkt##.flags & PCIE_DESC_TX_LSO)
+#define _ISSUE_PROC_LSO_TEST(_pkt)                                  \
+    ((tx_desc.pkt##_pkt##.__raw[2] &                                \
+      (PCIE_DESC_TX_LSO << NFD_IN_DMA_STATE_FLAGS_shf)) &&          \
+     (issue_dma_queue_state_bit_set_test(NFD_IN_DMA_STATE_UP_shf)))
 #else
 #define _ISSUE_PROC_LSO_TEST(_pkt) (0)
 #endif
