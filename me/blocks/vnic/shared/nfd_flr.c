3cf3216613d54d174aaf823c1f11a9a99661422c
Jakub Kicinski <jakub.kicinski@netronome.com>
2016-09-30 18:26:06 +0100

    Add FLR handling of the eBPF portion of the BAR

--- me/blocks/vnic/shared/nfd_flr.c
+++ me/blocks/vnic/shared/nfd_flr.c
@@ -165,6 +165,11 @@ nfd_flr_init_pf_ctrl_bar(__emem char *isl_base, unsigned int vnic)
                                    NFD_NATQ2QC(q_base, NFD_OUT_FL_QUEUE)};
     __xwrite unsigned int exn_lsc = 0xffffffff;
     __xwrite unsigned int rx_off = NFD_OUT_RX_OFFSET;
+#ifdef NFD_BPF_CAPABLE
+    __xwrite unsigned int bpf_cfg[] = { NFP_NET_BPF_ABI | (8 * 1024 - NFD_BPF_START_OFF) << 16,
+                                        NFD_BPF_START_OFF | NFD_BPF_DONE_OFF << 16,
+                                        30 << 8 /* CTM buf size / 64 */ };
+#endif
 
     mem_write64(&cfg,
                 NFD_CFG_BAR(isl_base, vnic) + NFP_NET_CFG_VERSION,
@@ -176,6 +181,11 @@ nfd_flr_init_pf_ctrl_bar(__emem char *isl_base, unsigned int vnic)
     mem_write8(&rx_off,
                NFD_CFG_BAR(isl_base, vnic) + NFP_NET_CFG_RX_OFFSET,
                sizeof rx_off);
+#ifdef NFD_BPF_CAPABLE
+    mem_write8(&bpf_cfg,
+               NFD_CFG_BAR_ISL(PCIE_ISL, NFD_MAX_VFS) + NFP_NET_CFG_BPF_ABI,
+               sizeof bpf_cfg);
+#endif
 #endif
 }
 
@@ -205,6 +215,11 @@ nfd_flr_init_vf_ctrl_bar(__emem char *isl_base, __emem char *vf_cfg_base, unsign
     __xwrite unsigned int rx_off = NFD_OUT_RX_OFFSET;
     __xread unsigned int vf_cfg_rd[2];
     __xwrite unsigned int vf_cfg_wr[2];
+#ifdef NFD_BPF_CAPABLE
+    __xwrite unsigned int bpf_cfg[] = { NFP_NET_BPF_ABI | (8 * 1024 - NFD_BPF_START_OFF) << 16,
+                                        NFD_BPF_START_OFF | NFD_BPF_DONE_OFF << 16,
+                                        30 << 8 /* CTM buf size / 64 */ };
+#endif
 
 
     mem_write64(&cfg, NFD_CFG_BAR(isl_base, vf) + NFP_NET_CFG_VERSION,
@@ -222,6 +237,11 @@ nfd_flr_init_vf_ctrl_bar(__emem char *isl_base, __emem char *vf_cfg_base, unsign
     mem_write8(&vf_cfg_wr, NFD_CFG_BAR(isl_base, vf) + NFP_NET_CFG_MACADDR,
               NFD_VF_CFG_MAC_SZ);
 
+#ifdef NFD_BPF_CAPABLE
+    mem_write8(&bpf_cfg,
+               NFD_CFG_BAR_ISL(PCIE_ISL, NFD_MAX_VFS) + NFP_NET_CFG_BPF_ABI,
+               sizeof bpf_cfg);
+#endif
 #endif
 }
 
